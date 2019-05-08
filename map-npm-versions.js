95ba73fe8d1128cc286e5dc8dfc1282b266761e5
Evgeny <zag7moon@gmail.com>
2019-05-08 14:52:55 +0700

    map npm versions

---
+++ map-npm-versions.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var p = JSON.parse( fs.readFileSync( 'package.json') );
+var v = JSON.parse( fs.readFileSync( 'npm-shrinkwrap.json') );
+
+updateDependencies( p.dependencies,    v.dependencies );
+updateDependencies( p.devDependencies, v.dependencies );
+
+fs.writeFileSync( 'package-lockdown.json', JSON.stringify( p, null, 2 ) );
+
+function updateDependencies( list, v )
+{
+        for ( var d in list )
+                list[d] = v[d].version;
+}
